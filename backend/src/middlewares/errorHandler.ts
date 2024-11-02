1fe4971fb1de3d25a1799fcac7913cc42f9738f2
Hinkywan <111245150+Hinkywan@users.noreply.github.com>
2024-11-03 00:43:57 +0800

    fix(BE)
    - import error

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../utils/CustomError';
+import CustomError from '../utils/CustomError';
 
 // Global error handler
 const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
